e2e7b0ae3e2c743c2b0f7d82f56a1e9cf273ffdc
smiley88888 <wannarichman88888@gmail.com>
2024-07-24 06:31:17 -0400

    add a new field(Rating)

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -5,10 +5,13 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int Id { get; set; } 
-        public string? Title { get; set; }
+        public string? Title { get; set; } = string.Empty;
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        public string? Genre { get; set; }
+        public string? Genre { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        public string Rating { get; set; } = string.Empty;
+
+
     }
 }
